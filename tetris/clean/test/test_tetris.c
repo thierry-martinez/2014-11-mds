af581977e9e5bf4f098ccecb911f0c7bd5095400
training <training@mdls27>
2014-11-04 17:35:00 +0100

    Test of set_grid_to_zero function

--- tetris/clean/test/test_tetris.c
+++ tetris/clean/test/test_tetris.c
@@ -1,31 +1,7 @@
- /*
- *  This program (crudely) demonstrates a very simple "black box"
- *  test of the standard library functions fprintf() and fread().
- *  It uses suite initialization and cleanup functions to open
- *  and close a common temporary file used by the test functions.
- *  The test functions then write to and read from the temporary
- *  file in the course of testing the library functions.
- *
- *  The 2 test functions are added to a single CUnit suite, and
- *  then run using the CUnit Basic interface.  The output of the
- *  program (on CUnit version 2.0-2) is:
- *
- *           CUnit : A Unit testing framework for C.
- *           http://cunit.sourceforge.net/
- *
- *       Suite: Suite_1
- *         Test: test of fprintf() ... passed
- *         Test: test of fread() ... passed
- *
- *       --Run Summary: Type      Total     Ran  Passed  Failed
- *                      suites        1       1     n/a       0
- *                      tests         2       2       2       0
- *                      asserts       5       5       5       0
- */
-
 #include <stdio.h>
 #include <string.h>
 #include "CUnit/Basic.h"
+#include "../grid.h"
 
 /* Pointer to the file used by the tests. */
 static FILE* temp_file = NULL;
@@ -34,60 +10,31 @@ static FILE* temp_file = NULL;
  * Opens the temporary file used by the tests.
  * Returns zero on success, non-zero otherwise.
  */
-int init_suite1(void)
+int init_suite(void)
 {
-   if (NULL == (temp_file = fopen("temp.txt", "w+"))) {
-      return -1;
-   }
-   else {
-      return 0;
-   }
+   return 0;
 }
 
 /* The suite cleanup function.
  * Closes the temporary file used by the tests.
  * Returns zero on success, non-zero otherwise.
  */
-int clean_suite1(void)
+int clean_suite(void)
 {
-   if (0 != fclose(temp_file)) {
-      return -1;
-   }
-   else {
-      temp_file = NULL;
-      return 0;
-   }
+   return 0;
 }
 
-/* Simple test of fprintf().
- * Writes test data to the temporary file and checks
- * whether the expected number of bytes were written.
- */
-void testFPRINTF(void)
+void test_set_grid_to_zero(void)
 {
-   int i1 = 10;
+  int row_index, column_index;
 
-   if (NULL != temp_file) {
-      CU_ASSERT(0 == fprintf(temp_file, ""));
-      CU_ASSERT(2 == fprintf(temp_file, "Q\n"));
-      CU_ASSERT(7 == fprintf(temp_file, "i1 = %d", i1));
-   }
-}
+  set_grid_to_zero();
 
-/* Simple test of fread().
- * Reads the data previously written by testFPRINTF()
- * and checks whether the expected characters are present.
- * Must be run after testFPRINTF().
- */
-void testFREAD(void)
-{
-   unsigned char buffer[20];
-
-   if (NULL != temp_file) {
-      rewind(temp_file);
-      CU_ASSERT(9 == fread(buffer, sizeof(unsigned char), 20, temp_file));
-      CU_ASSERT(0 == strncmp(buffer, "Q\ni1 = 10", 9));
-   }
+  for ( row_index = 0 ; row_index < NUMBER_OF_ROWS ; ++row_index ) {
+    for ( column_index = 0 ; column_index < NUMBER_OF_COLUMNS ; ++column_index ) {
+      CU_ASSERT( grid[row_index][column_index] == 0 );       
+    }
+  }
 }
 
 /* The main() function for setting up and running the tests.
@@ -103,7 +50,7 @@ int main()
       return CU_get_error();
 
    /* add a suite to the registry */
-   pSuite = CU_add_suite("Suite_1", init_suite1, clean_suite1);
+   pSuite = CU_add_suite("Suite_grid", init_suite, clean_suite);
    if (NULL == pSuite) {
       CU_cleanup_registry();
       return CU_get_error();
@@ -111,8 +58,7 @@ int main()
 
    /* add the tests to the suite */
    /* NOTE - ORDER IS IMPORTANT - MUST TEST fread() AFTER fprintf() */
-   if ((NULL == CU_add_test(pSuite, "test of fprintf()", testFPRINTF)) ||
-       (NULL == CU_add_test(pSuite, "test of fread()", testFREAD)))
+   if (NULL == CU_add_test(pSuite, "test of set_grid_to_zero()", test_set_grid_to_zero))
    {
       CU_cleanup_registry();
       return CU_get_error();
